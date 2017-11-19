4959c680aa9182a0896762cb4b1a383479eebdc5
Gaétan Deglorie <g.deglorie@gmail.com>
2017-11-19 12:48:22 +0100

    Add linking against GLFW using packages (fixes #1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
 int main()
 {
+    if (!glfwInit()) {
+        std::cerr << "Can't initialize GLFW" << std::endl;
+        exit (EXIT_FAILURE);
+    } else {
+        std::cout << "Success" << std::endl;
+    }
+
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
